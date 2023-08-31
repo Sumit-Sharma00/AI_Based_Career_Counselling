7da618056efef1d7fa79e45c70ac84e04b5013d0
Sumit-Sharma00 <142771342+Sumit-Sharma00@users.noreply.github.com>
2023-08-31 22:50:24 +0530

    Add files via upload

---
+++ ai.py
@@ -0,0 +1,172 @@
+import pandas as pd
+import sys
+print("****WELCOME TO THE AI BASED CAREER COUNSELLING****")
+
+
+def BCA():
+    print("Sure, Here is what you need to get enroll in a BCA degree \n\nBCA or Bachelor of Computer Application is a three-year undergraduate degree program that provides students with a solid background in computer science, programming languages, software development, and other related fields. After completion of the course, graduates can opt for jobs like technical associate, blockchain developer, data manager, etc in both private and public companies. \n \n          BCA Course Details \n Degree            	Bachelors \n Full Form         	Bachelor of Computer Application \n Duration          	3 Years \n Age               	No age limit \n Minimum Percentage	Minimum of 45% marks in Higher Secondary \n Average Fees      	INR 15,000 - 2 LPA \n Average Salary    	INR 3.7 LPA [Source: Payscale] \n Employment Roles  	Software Developer, Technical Analyst, Technical Support, Networking Trainee, Server Administrator etc.\n")
+    agn=str(input("\n\nDo You want to continue from Start Again ?"))
+    if agn in ("yes","YES","Yes","y"):
+        start()
+    else:
+        sys.exit()
+
+def BTECH():
+    print("Sure, Here is what you need to get enroll in a B.Tech degree \n\nB.Tech or Bachelor of Technology is a Four-year undergraduate degree program that provides students with a solid background in computer science, programming languages, software development, and other related fields. After completion of the course, graduates can opt for jobs like technical associate, blockchain developer, data manager, etc in both private and public companies. \n \n          B.Tech Course Details \n Degree                   Bachelors \n Full Form                Bachelor of Technology \n Duration                 4 years \n Entrance Exams           JEE Main, JEE Advanced, BITSAT, VITEEE, LPUNEST, IPU CET, SHIATS, SRMJEE and AMITY JEE \n Eligibility Criteria     10+2 PCM or PCB from a recognized Board in India \n Top Institutes           IITs, IIITs, NITs, GFTIs as well as private institutes such as BITS, VIT, MIT, etc.\n Top Recruiters           Google, Apple, TATA, ISRO, Microsoft, Amazon, Flipkart, etc\n Job Opportunites         Software Engineer, Data Scientist, Mechanical Engineer, Aeronautical Engineer, Marine Engineer, Automobile Engineer, Civil Engineer, IT Consultant, SAP Consultant, Electrical Engineer, etc\n")
+    agn=str(input("\n\nDo You want to continue from Start Again ?"))
+    if agn in ("yes","YES","Yes","y"):
+        start()
+    else:
+        sys.exit()
+
+def BSC_CS():
+     print("Sure, Here is what you need to get enroll in a BSc CS degree \n\nBSc CS or Bachelor of Science in Computer Science is a Three-year undergraduate degree program that provides students with a solid background in computer science, programming languages, software development, and other related fields. After completion of the course, graduates can opt for jobs like technical associate, blockchain developer, data manager, etc in both private and public companies. \n \n          BSc CS Course Details \n Degree               Bachelors \n Full-Form            Bachelor of Science in Computer Science \n Duration             Three Years divided into six semesters \n Eligibility          Pass in 10+2 with at least 45% marks \n Selection Process    On the basis of merit in qualifying examination (10+2) \n Average Courses Fee  Rs 10,000 - Rs 25,000 per year \n Average Salary       Rs. 1 LPA - Rs 3 LPA \n Top Recruiters       TCS, Infosys, HDFC Bank, IBM, Wipro, Capgemini\n")
+     agn=str(input("\n\nDo You want to continue from Start Again ?"))
+     if agn in ("yes","YES","Yes","y"):
+         start()
+     else:
+         sys.exit()
+ 
+def BSC_IT():
+    print("Sure, Here is what you need to get enroll in a BSc IT degree \n\nBSc IT or Bachelor of Science in Information Technology is a Three-year undergraduate degree program that provides students with a solid background in computer science, programming languages, software development, and other related fields. After completion of the course, graduates can opt for jobs like technical associate, blockchain developer, data manager, etc in both private and public companies. \n \n          BSc IT Course Details \n Degree                 Bachelors \n Full-Form              Bachelor on Science in Information Technology \n Duration               3 years \n Entrance Exams         NEST, IISER Aptitude Test, UPCATET Exam, GSAT \n Eligibility Criteria   Completion of 10+2 in the science stream with a minimum of 50 per cent of marks \n Admission process      Entrance/Merit \n Fees                   Rs. 80,000 to Rs. 4,00,000 \n Average Salary         Rs. 200,000 to Rs. 6,00,000 \n Job profiles           Graphic Designer, Software Developer, Computer Hardware Engineer, Network Technician \n Top recruiters         TCS, IBM, Accenture, Infosys, Cognizant\n")
+    agn=str(input("\n\nDo You want to continue from Start Again ?"))
+    if agn in ("yes","YES","Yes","y"):
+        start()
+    else:
+        sys.exit()
+
+def MBBS():
+    print("Sure, Here is what you need to get enroll in a MBBS degree \n\nThe MBBS full form is Bachelor of Medicine, Bachelor of Surgery. The MBBS course duration is 5 years. MBBS is a UG course for aspirants who wish to fulfil their dream of becoming a doctor. It is a professional degree in Medical Science. \n\n          MBBS Course Details\n Full Form            Bachelor of Medicine, Bachelor of Surgery \n Degree               Bachelors \n Course Duration      5 years \n Admission Process    NEET + Counselling \n Course Fees          INR 25,000 to INR 1.15 crore \n Entrance Exam        NEET \n Top Colleges         AIIMS Delhi, Christian Medical College Vellore, Banaras Hindu University, JIPMER Puducherry, etc. \n Job Profiles         Medical Surgeon, Medical officers, Paediatrician, General Physician, Medical Assistant (Surgery), etc. \n Average Salary       INR 2.5 LPA - INR 15 LPA ")
+    agn=str(input("\n\nDo You want to continue from Start Again ?"))
+    if agn in ("yes","YES","Yes","y"):
+        start()
+    else:
+        sys.exit()
+
+def nursing():
+    print(" Sure, Here is what you need to get enroll in a BSc Nursing degree \n\nB.Sc Nursing full form is Bachelor of Science in Nursing.It is a UG course. Medicine is one of the few fields that will always exist.\n\n         BSc Nursing Course Details\n Degree	               Bachelors \n Full Form             Bachelor of Science in Nursing \n Course Duration       4 Years. \n Age limit             no age limit \n Minimum Percentage    50% in 10+2 from a recognised board \n Course Fees           INR 20,000 - 2,50,000 LPA \n Salary in India       INR 3 LPA \n Employment Roles	     Staff Nurse, Registered Nurse (RN), Medical Coder, Nurse - Intensive Care Unit (ICU), Neonatal Intensive Care Unit (NICU) Registered Nurse etc")
+    agn=str(input("\n\nDo You want to continue from Start Again ?"))
+    if agn in ("yes","YES","Yes","y"):
+        start()
+    else:
+        sys.exit()
+
+def BPharma():
+    print(" Sure, Here is what you need to get enroll in a B.Pharma degree \n\nB.Pharma full form is Bachelor of Pharmacy.It is a UG course. Medicine is one of the few fields that will always exist.\n\n         B.Pharma Course Details\n Degree                 Bachelors \n Course Name            Bachelor of Pharmacy \n Eligibility            10+2 with Science Biology/Mathematics \n Duration               Four years \n Average Fee            Rs. 50,000 to Rs. 22,00,000 \n Average Salary         Rs. 5 LPA \n Career Opportunities   Pharmacist, Chemist, Biochemist \n Top Recruiters         Cipla, Reddy’s Laboratories, Lupin, Sun Pharmaceutical Ltd, Aurobindo Pharma., Zydus Cadila, Torrent Pharmaceuticals,etc.")
+    agn=str(input("\n\nDo You want to continue from Start Again ?"))
+    if agn in ("yes","YES","Yes","y"):
+        start()
+    else:
+        sys.exit()
+
+def Teacher():
+    print(" Sure, Here is what you need to get enroll in a B.Ed degree \n\nThe full form of B Ed is “Bachelor of Education”. According to the National Council for Teacher Education (NCTE), completing B.Ed is a requirement for teaching higher education in schools. B.Ed is a two-year professional course and not an undergraduate program. It involves theoretical studies, assignments, fieldwork, and research.\n\n         B.Ed Course Details\n Degree                 Bachelors \n Course Name            Bachelor of Education \n Eligibility            10+2 with Min. 50% from any recognised Board \n Course Duration        2 years \n Education Mode         Full-Time/ Distance \n Course Fee	            INR 40,000- 2 lakh \n Top Colleges           Delhi University, IGNOU, Amity University, Banaras Hindu University, Andhra University \n Average Salary         INR 3.5 LPA- INR 6.5 LPA \n Job Profiles           Trained Graduate Teacher, Post Graduate Teacher, Secondary Teacher, Vice-Principal, Principal, Content Developer, Subject Matter \n Entrance Exams         RIE CEE, PTET, DUET, IGNOU B.Ed Entrance \n Top Recruiters         Navodaya Vidyalayas, Kendriya Vidyalayas, Adarsha Vidyalayas, Private schools")
+    agn=str(input("\n\nDo You want to continue from Start Again ?"))
+    if agn in ("yes","YES","Yes","y"):
+        start()
+    else:
+        sys.exit()
+
+    
+
+def school():
+    feild=str(input("\nWhich feild (Technical/Health Care/ Teaching) do you want to pursue : "))
+
+    if feild==("technical") or feild==("Technical") :
+        print("\nGreat choice, here are the courses for technical feild : ")
+        print("1. B.Tech" '\n' "2. BCA" '\n' "3. BSc in CS" '\n' "4. BSC in IT\n")
+        technical=str(input("Which degree do you want to pursue : "))
+
+        if technical==("BCA"):
+            BCA()
+
+        elif technical==("B.Tech") or technical==("Btech"):
+            BTECH()
+
+        elif technical in ("BSC CS", "BSC cs", "bsc CS","BSC cs","Bsc CS","Bsc cs","BSc CS","BSc in CS","Bsc in CS","BSC in CS"):
+            BSC_CS()
+
+        elif technical in ("BSC IT", "BSc IT","BSC it","Bsc IT","Bsc it","BSc in IT","Bsc in IT","BSC in IT"):
+            BSC_IT()
+
+        else:
+            print("Please Enter A Valid Degree Course From Above Options")
+
+
+    elif feild in ("Health Care","Health care","health care"):
+        print("\nGreat choice, here are the courses for Health Care feild : ")
+        print("1. MBBS" '\n' "2. BSC Nursing" '\n' "3. B.Pharma" '\n')
+        health=str(input("Which degree do you want to pursue : "))
+
+        if health==("MBBS")or health==("mbbs"):
+            MBBS()
+
+        elif health in ("BSc Nursing","BSC NURSING","Bsc nursing","Bsc Nursing","bsc nursing","nursing","Nursing","BSC Nursing","BSC nursing"):
+            nursing()
+
+        elif health in ("B.Pharma","BPharma","Bpharma","B pharma","bpharma","B Pharma","B. Pharma","B pharma"):
+            BPharma()
+
+        else:
+            print("Please Enter A Valid Degree Course From Above Options")
+
+
+    elif feild in("Teaching","teaching","Teacher","teacher"):
+        print("\nGreat choice, here are the details of B.Ed course for Teaching feild : ")
+        Teacher()
+
+
+    else:
+        print("Please select one from the given Field. The rest are under Construction")
+
+
+
+def Direct():
+    print("Please Select (Write) A Degree From Given Below Options:")
+    print("1.BCA\n2.B.Tech\n3.BSc CS\n4.BSc IT\n5.MBBS\n6.BSc Nursing\n7.B.Pharma\n8.Teaching")
+    D=input("Enter The Degree You want To Enquire About : ")
+    if D==("BCA"):
+        BCA()
+
+    elif D==("B.Tech") or D==("Btech"):
+        BTECH()
+
+    elif D in ("BSC CS", "BSC cs", "bsc CS","BSC cs","Bsc CS","Bsc cs","BSc CS","BSc in CS","Bsc in CS","BSC in CS"):
+        BSC_CS()
+
+    elif D in ("BSC IT", "BSc IT","BSC it","Bsc IT","Bsc it","BSc in IT","Bsc in IT","BSC in IT"):
+        BSC_IT()
+        
+    elif D==("MBBS")or D==("mbbs"):
+        MBBS()
+
+    elif D in ("BSc Nursing","BSC NURSING","Bsc nursing","Bsc Nursing","bsc nursing","nursing","Nursing","BSC Nursing","BSC nursing"):
+        nursing()
+
+    elif D in ("B.Pharma","BPharma","Bpharma","B pharma","bpharma","B Pharma","B. Pharma","B pharma"):
+        BPharma()
+
+    elif D in("Teaching","teaching","Teacher","teacher"):
+        Teacher()
+
+    else:
+        print("Please select one from the given Field. The rest are under Construction")
+
+
+        
+        
+        
+                        
+       
+def start():
+    choice=str(input("\nSo, you are a (please type your answer) : "'\n''\n' "1. School Student"'\n'"2. Direct enquire about a degree\n"))
+    if choice in ("School Student","School student","school student"):
+        school()
+    elif choice in ("Direct enquiry about a degree","Direct enquire about a degree","direct enquiry about a degree","Direct enquiry","direct enquire","Direct","direct"):
+        Direct()
+    else:
+        print("Please Eter A Valid Field : ")
+        start()
+
+start()
+
